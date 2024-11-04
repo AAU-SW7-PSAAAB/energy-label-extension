a11ab6f598b5c8d245cef0b11ec65b408a2f6803
Thrane <simont1998@gmail.com>
2024-11-04 11:41:10 +0100

    atempts to get background color

--- source/plugins/darkmode.ts
+++ source/plugins/darkmode.ts
@@ -9,15 +9,30 @@ class DarkmodePlugin implements IPlugin {
         const body = input.dom("body");
         debug.debug(css)
         debug.debug("tetstst")
-        debug.debug(body)
-
+        // debug.debug(body)
+        debug.debug(input.dom("[color-scheme]"))
+        debug.debug(input.dom("[color-scheme]=dark"))
+        debug.debug(getComputedStyle(document.documentElement).backgroundColor) //I think this gets expansion document
+        debug.debug(input.dom("body").css("backgroundColor"))
+        debug.debug(getComputedStyle(document.body).backgroundColor)
+        // debug.debug(getComputedStyle(document.querySelector(':root'))["color-scheme"])//this gets "normal" instead of dark in extension?
+        debug.debug(input.dom(':root'))
         if(
             body.attr("data-dark-mode") ||
             css.search(/@media \(prefers-color-scheme: dark\)/) >= 0 ||
             css.search(/html\[dark\]/) >= 0 || //youtube
-            css.search(/:root.dark/) >= 0 //svelte
+            css.search(/:root.dark/) >= 0 ||//svelte
+            css.search(/color-scheme: dark/) >= 0 || //twitter might not work - twitter uses auto...
+            css.search(/color-scheme: auto/) >= 0 //twitter might not work - twitter uses auto...
         )
+        {
+            debug.debug(css.search(/@media \(prefers-color-scheme: dark\)/) >= 0)
+            debug.debug(css.search(/html\[dark\]/) >= 0)
+            debug.debug(css.search(/:root.dark/) >= 0)
+            debug.debug(css.search(/color-scheme: dark/) >= 0)
+            debug.debug(css.search(/color-scheme: auto/) >= 0)
             return 1
+        }
 		return 0
 	}
 }
