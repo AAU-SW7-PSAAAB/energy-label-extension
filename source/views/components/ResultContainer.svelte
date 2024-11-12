ac2f5abd14583532ae3a7aeddbffd3a55355ab96
Anton <dev@atjn.dk>
2024-11-12 09:14:24 +0100

    Also format svelte files (#79)

--- source/views/components/ResultContainer.svelte
+++ source/views/components/ResultContainer.svelte
@@ -1,26 +1,29 @@
 <script lang="ts">
-	import type { Snippet } from 'svelte';
-  let { header = $bindable(), children }: { header: string | null, children: Snippet } = $props();
+	import type { Snippet } from "svelte";
+	let {
+		header = $bindable(),
+		children,
+	}: { header: string | null; children: Snippet } = $props();
 </script>
 
-{#if (header !== null)}
-<h3 class="container-header">{header}</h3>
+{#if header !== null}
+	<h3 class="container-header">{header}</h3>
 {/if}
 <div class="container">
-  {@render children()}
+	{@render children()}
 </div>
 
 <style>
-  .container-header {
-    margin-bottom: 4px;
-    margin-left: 10px;
-  }
-  .container {
-  background-color: rgb(239, 239, 239);
+	.container-header {
+		margin-bottom: 4px;
+		margin-left: 10px;
+	}
+	.container {
+		background-color: rgb(239, 239, 239);
 
-  padding: 10px;
-  margin-top: none;
-  margin-bottom: 10px;
-  border-radius: 10px;
-}
+		padding: 10px;
+		margin-top: none;
+		margin-bottom: 10px;
+		border-radius: 10px;
+	}
 </style>
