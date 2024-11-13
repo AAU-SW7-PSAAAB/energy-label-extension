fca0335fa2600768d76de0096fd80a8d359b6765
DenFlyvendeGed <simont1998@gmail.com>
2024-11-13 09:50:15 +0100

    Added comments

--- source/plugins/darkmode.ts
+++ source/plugins/darkmode.ts
@@ -13,39 +13,35 @@ class DarkmodePlugin implements IPlugin {
         const parser = new DOMParser();
         const document = parser.parseFromString(dom, "text/html");
         const body = document.querySelector("body")
-        if (body) {
-            
-            const numberstr: string[]  = body.style.background.match(/\d+/g)?? [""];
-            let numbers = numberstr.map((number) => {return parseInt(number)})
-            let brightness = 255
-            if(numbers?.length){
-                //best way to calculate brightness acording to google
-                brightness = numbers[0] *0.2126 + numbers[1] * 0.7152 + numbers[2] * 0.0722
-            }
-            
-            debug.debug(body.style.background)   
-            debug.debug(css)       
-
-            if(
-                body.getAttribute("data-dark-mode") ||
-                css.search(/@media \(prefers-color-scheme: dark\)/) >= 0 ||
-                css.search(/html\[dark\]/) >= 0 || //youtube
-                css.search(/:root.dark/) >= 0 ||//svelte
-                brightness < 128
-                
-            )
-            {
-                debug.debug(css.search(/@media \(prefers-color-scheme: dark\)/) >= 0)
-                debug.debug(css.search(/html\[dark\]/) >= 0)
-                debug.debug(css.search(/:root.dark/) >= 0)
-                return 100
-            }
+        if (!body) {
+            debug.error("Darkmode plugin failed to load body dom")
             return 0
         }
-        else
-            debug.error("Failed to load body dom")
-        return -1
         
+        const numberstr: string[]  = body.style.background.match(/\d+/g)?? [""]; // "rgb(112,24,0)" -> ["112", "24", "0"]
+        let numbers = numberstr.map((number) => {return parseInt(number)})
+        let brightness = 255 //Default brightness to white incase background is not defined
+        if(numbers?.length){
+            //best way to calculate brightness acording to google
+            brightness = numbers[0] *0.2126 + numbers[1] * 0.7152 + numbers[2] * 0.0722
+        }
+             
+
+        if(
+            body.getAttribute("data-dark-mode") ||
+            css.search(/@media \(prefers-color-scheme: dark\)/) >= 0 || //Browser theme check rule
+            css.search(/html\[dark\]/) >= 0 || //youtube
+            css.search(/:root.dark/) >= 0 ||//svelte
+            brightness < 128 //dark by design websites
+            
+        )
+        {
+            debug.debug(css.search(/@media \(prefers-color-scheme: dark\)/) >= 0)
+            debug.debug(css.search(/html\[dark\]/) >= 0)
+            debug.debug(css.search(/:root.dark/) >= 0)
+            return 100
+        }
+        return 0
 	}
 }
 
