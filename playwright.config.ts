44595b7e6cdb2cd382fdd58032b5b518050fa34a
NybyDK <nyby.dev@gmail.com>
2024-11-19 11:39:44 +0100

    haha gone

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,9 +2,6 @@ import type { PlaywrightTestConfig } from "@playwright/test";
 import { devices } from "@playwright/test";
 
 const config: PlaywrightTestConfig = {
-	webServer: {
-		command: "vite preview",
-	},
 	testDir: "tests/e2e",
 	testMatch: /(.+\.)?test\.ts/,
 	projects: [
