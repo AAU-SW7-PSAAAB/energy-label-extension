fc1054b58f2595d0a275dc930f6ee57efb34f40f
Anton <dev@atjn.dk>
2024-11-28 12:51:50 +0100

    Help the user fix the website (#164)

--- source/views/components/CheckContainer.svelte
+++ source/views/components/CheckContainer.svelte
@@ -11,6 +11,14 @@
 <details class={check.type} data-check-name={check.name}>
 	<summary>{check.name} - {check.score}</summary>
 	<p>{check.description}</p>
+	{#if check.susWebLink}
+		<p>
+			You can read more about this optimization at Sustainable Web Design: <a
+				target="_blank"
+				href={check.susWebLink}>{check.susWebLink}</a
+			>
+		</p>
+	{/if}
 	{#if check.table}
 		<TableView rows={check.table}></TableView>
 	{/if}
