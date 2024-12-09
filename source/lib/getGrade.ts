22b6e6ad4233cbc37e68200283d52f61db5b13ff
NybyDK <nyby.dev@gmail.com>
2024-12-09 09:25:04 +0100

    hehe

--- source/lib/getGrade.ts
+++ source/lib/getGrade.ts
@@ -1,8 +1,5 @@
+const grades = ["G", "F", "E", "D", "C", "B", "A"];
+
 export default function getGrade(score: number) {
-	if (score >= 100) return "A";
-	if (score >= 80) return "B";
-	if (score >= 60) return "C";
-	if (score >= 40) return "D";
-	if (score >= 20) return "E";
-	return "F";
+	return grades[Math.min(Math.floor(score / (100 / grades.length)), 6)];
 }
